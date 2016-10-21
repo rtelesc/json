f841097cd52c54afb4ac3ed6b19ddb44f42595e4
Rodrigo Teles <rtelesc@gmail.com>
2016-10-20 23:15:31 -0200

    upload repositorio

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# json
-Tutorial básico para integração de ajax + php parser
+# Tutorial Ajax + Json + PHP
+código básico de introdução para ajax + php
+
+
+#Autor
+Rodrigo Teles
+
+
+Enjoy! =)
+
+
---
+++ receive.php
@@ -0,0 +1,8 @@
+<?php
+
+    $data = json_decode(file_get_contents('php://input'), true); //recebe json postado pelo oajax
+
+
+    $data = (array) $data; //  converte o json em um array para poder usar o comando ->field;
+
+    echo $data->nome;
